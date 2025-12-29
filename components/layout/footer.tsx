ef5e6b1ba266ffd1884a98d576d92a61d3c54275
Thiago Poderoso <tpoderoso@gmail.com>
2025-12-29 15:51:38 -0300

    feat: enhance About page with Avatar component; update NavBar and Footer links; add new Articles, Downloads, and Tips pages; update dependencies

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -6,10 +6,16 @@ export default function Footer() {
       <ul className="flex justify-between">
         <li className="text-sm text-pink-400 font-extralight">Versão 0.0.1</li>
         <li>
-          <CodeXml
-            size={20}
-            className="hover:text-purple-400 transition-colors duration-300 text-gray-300"
-          />
+          <a
+            href="https://www.github.com/tpoderoso/poderoso.io"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <CodeXml
+              size={20}
+              className="hover:text-purple-400 transition-colors duration-300 text-gray-300"
+            />
+          </a>
         </li>
       </ul>
     </footer>
--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,33 +1,56 @@
-import { Download, FolderDown, Mail } from "lucide-react";
+import { FolderDown, Mail } from "lucide-react";
+import Link from "next/link";
 import { SiGithub, SiLinkedin } from "react-icons/si";
 
 export default function NavBar() {
   return (
     <nav className="flex justify-between gap-3 w-full">
       <div>
-        <p className="text-purple-400 font-extrabold">Poderoso.IO</p>
+        <p className="text-purple-400 font-extrabold">
+          <Link href={"/"}>Thiago Poderoso</Link>{" "}
+        </p>
       </div>
       <ul className="flex items-center gap-6">
-        <li>Artigos</li>
-        <li>Dicas</li>
-        <li>Sobre</li>
-        <li>
-          <FolderDown
-            size={20}
-            className="hover:text-purple-400 transition-colors duration-300 text-gray-300 cursor-pointer"
-          />
+        <li className="hover:text-purple-400 transition-colors duration-300 text-gray-300 cursor-pointer">
+          <Link href={"/articles"}>Artigos</Link>
+        </li>
+        <li className="hover:text-purple-400 transition-colors duration-300 text-gray-300 cursor-pointer">
+          <Link href={"/tips"}>Dicas</Link>
+        </li>
+        <li className="hover:text-purple-400 transition-colors duration-300 text-gray-300 cursor-pointer">
+          <Link href={"/about"}>Sobre</Link>
         </li>
         <li>
-          <SiLinkedin
-            size={18}
-            className="hover:text-purple-400 transition-colors duration-300 text-gray-300 cursor-pointer"
-          />
+          <Link href={"/downloads"}>
+            <FolderDown
+              size={20}
+              className="hover:text-purple-400 transition-colors duration-300 text-gray-300 cursor-pointer"
+            />
+          </Link>
         </li>
         <li>
-          <SiGithub
-            size={20}
-            className="hover:text-purple-400 transition-colors duration-300 text-gray-300 cursor-pointer"
-          />
+          <a
+            href="https://www.linkedin.com/in/tpoderoso/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SiLinkedin
+              size={18}
+              className="hover:text-purple-400 transition-colors duration-300 text-gray-300 cursor-pointer"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.github.com/tpoderoso/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SiGithub
+              size={20}
+              className="hover:text-purple-400 transition-colors duration-300 text-gray-300 cursor-pointer"
+            />
+          </a>
         </li>
         <li>
           <Mail
